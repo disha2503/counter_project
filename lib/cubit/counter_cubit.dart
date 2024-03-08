fa03b1832c473b35c66b77d4e06170ff17b69896
Disha Ashok Shetty <109512147+disha2503@users.noreply.github.com>
2024-03-08 11:01:30 +0530

    counter project using bloc architecture

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(number: 0));
+
+  void increment() => emit(CounterState(number: state.number + 1));
+  void decrement() => emit(CounterState(number: state.number - 1));
+}
